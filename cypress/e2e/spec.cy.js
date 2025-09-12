5372ec475054247d8bc7fc267726f07baf29d30a
Vinhit07 <vinith11107@gmail.com>
2025-09-13 00:34:08 +0530

    Feat: added spec file for signin and signup page

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,33 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    // Visit the base URL before each test in this suite.
+    cy.visit('/signin');
+  });
+  it('correct password and username', () => {
+    cy.get('[name="email"]').type('pavan@gmail.com')
+    cy.get('[name="password"]').type('pavan123')
+    cy.get("[type='submit']").click()
+  })
+  it('wrong password', () => {
+    cy.get('[name="email"]').type('pavan@gmail.com')
+    cy.get('[name="password"]').type('pavana123')
+    cy.get("[type='submit']").click()
+  })
+  it('wrong username', () => {
+    cy.get('[name="email"]').type('pavana@gmail.com')
+    cy.get('[name="password"]').type('pavan123')
+    cy.get("[type='submit']").click()
+  })
+  it('visible sign in to your account',()=>{
+    cy.get('.mt-6.text-center.text-3xl.font-extrabold.text-gray-900').contains("Sign in to your account")
+    cy.get('.mt-2.text-center.text-sm.text-gray-600').contains("Or ", { matchCase: false })
+    cy.get('.font-medium.text-blue-600.hover\\:text-blue-500').contains("create a new account")
+  })
+  it('should show an error for empty fields when submitting', () => {
+        cy.get('button[type="submit"]').click();
+        cy.get('input[name="email"]:invalid').should('exist');
+        cy.get('input[name="password"]:invalid').should('exist');
+    });
+
+
+})
