733164b41ddf547763937e3500e60e15679a2f79
Lokeshwaran R <waranlokesh688@gmail.com>
2025-12-04 22:16:42 +0530

    fix: changed s3 bg image urls

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -87,7 +87,7 @@ const SignIn = () => {
             className="min-h-screen w-full flex items-center justify-center p-4"
             style={{
                 backgroundColor: 'black',
-                backgroundImage: `url('https://buvanesh-adya.s3.eu-north-1.amazonaws.com/stbg.png')`,
+                backgroundImage: `url('https://mrkadalai-files.s3.ap-south-1.amazonaws.com/stbg.png')`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no-repeat'
@@ -95,7 +95,7 @@ const SignIn = () => {
         >
             <div className="bg-white p-8 sm:p-12 rounded-2xl shadow-xl w-full max-w-md">
                 <div className="text-center">
-                    <img src="https://buvanesh-adya.s3.eu-north-1.amazonaws.com/logo2.jpg" alt="Logo" className="mx-auto h-20 w-auto mb-4" />
+                    <img src="https://mrkadalai-files.s3.ap-south-1.amazonaws.com/logo2.jpg" alt="Logo" className="mx-auto h-20 w-auto mb-4" />
                     <h2 className="text-3xl font-bold text-gray-900 mb-8">
                         Welcome Back
                     </h2>
